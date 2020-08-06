11ce633f4cc4e0d71467dc4c32a7561755e9f863
heyimmatt <mrmcfarland@gmail.com>
2020-08-06 13:16:05 -0700

    Add model for users

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 """Models for Blogly."""
-
+from app import app
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -10,3 +10,34 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+class User(db.Model):
+    """User Model"""
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,
+                  primary_key=True,
+                  autoincrement=True)
+    first_name = db.Column(db.String(50),
+                          nullable=False)
+    last_name = db.Column(db.String(50),
+                          nullable=False)
+    img_url = db.Column(db.String(), 
+                        nullable=False,
+                        default=f'https://api.adorable.io/avatars/150/{id}.png')
+    
+    def get_full_name(self): 
+        """Return user's full name"""
+        u = self
+        return f'{u.first_name} {u.last_name}'
+
+    def __repr__(self):
+        """Show user info"""
+        u = self
+        return f'<User {u.id} {u.first_name} {u.last_name} {u.img_url}>'
+
+    # @classmethod
+    # def get_user_by_last_name(cls, last_name):
+    #     """Returns list of users ordered by last name"""
+
+    #     return 
